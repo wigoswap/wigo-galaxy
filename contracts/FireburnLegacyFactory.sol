d390abb67901b955850e2e4d051229cc26fe8b0c
Chef Satoshi <chefsatoshi@proton.me>
2024-11-21 21:01:27 +0900

    added fireburn legacy challenge contract

---
+++ contracts/FireburnLegacyFactory.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "./OpenZeppelin/access/Ownable.sol";
+import "./OpenZeppelin/math/SafeMath.sol";
+import "./interfaces/IWigoGalaxy.sol";
+import "./OpenZeppelin/token/ERC20/IERC20.sol";
+import "./interfaces/IWiggyMinter.sol";
+
+/**
+ * @title Fireburn Legacy
+ * @notice User has to have at least 100M LUMOS token in its wallet.
+ */
+contract FireburnLegalcyFactory is Ownable {
+    using SafeMath for uint256;
+
+    IWiggyMinter public wiggyMinter;
+    IWigoGalaxy public wigoGalaxy;
+    IERC20 public token;
+
+    // WigoGalaxy related
+    uint256 public numberPoints;
+    uint256 public campaignId;
+    uint256 public thresholdBalance;
+
+    // WiggyMinter related
+    uint256 public endBlockTime;
+    string public tokenURI;
+    uint8 public constant wiggyId = 44;
+
+    event WiggyMint(
+        address indexed to,
+        uint256 indexed tokenId,
+        uint8 indexed wiggyId
+    );
+
+    // Map if address has already claimed a quest
+    mapping(address => bool) public hasClaimed;
+
+    event IncreasePoint(address indexed to, uint256 indexed point);
+
+    constructor(
+        address _wiggyMinter,
+        address _wigoGalaxy,
+        uint256 _endBlockTime,
+        IERC20 _token,
+        uint256 _thresholdBalance,
+        uint256 _numberPoints,
+        uint256 _campaignId,
+        string memory _tokenURI
+    ) public {
+        wiggyMinter = IWiggyMinter(_wiggyMinter);
+        wigoGalaxy = IWigoGalaxy(_wigoGalaxy);
+        endBlockTime = _endBlockTime;
+        token = _token;
+        thresholdBalance = _thresholdBalance;
+        numberPoints = _numberPoints;
+        campaignId = _campaignId;
+        tokenURI = _tokenURI;
+    }
+
+    /**
+     * @notice Mint a Wiggy from the WiggyMinter contract.
+     * @dev Users can claim once.
+     */
+    function mintNFT() external {
+        // Check whether quest is expired or not
+        require(block.timestamp <= endBlockTime, "TOO_LATE");
+
+        // Check that msg.sender has not claimed
+        require(!hasClaimed[msg.sender], "ERR_HAS_CLAIMED");
+
+        bool isUserActive;
+        (, , , , , , isUserActive) = wigoGalaxy.getResidentProfile(msg.sender);
+
+        // Check that msg.sender has an active profile
+        require(isUserActive, "ERR_USER_NOT_ACTIVE");
+
+        bool isUserEligible;
+        isUserEligible = _canClaim(msg.sender);
+
+        // Check that msg.sender is eligible
+        require(isUserEligible, "ERR_USER_NOT_ELIGIBLE");
+
+        // Update that msg.sender has claimed
+        hasClaimed[msg.sender] = true;
+
+        // Mint Wiggy and send it to the user.
+        uint256 tokenId = wiggyMinter.mintCollectible(
+            msg.sender,
+            tokenURI,
+            wiggyId
+        );
+
+        // Increase point on WigoGalaxy.
+        wigoGalaxy.increaseResidentPoints(
+            msg.sender,
+            numberPoints,
+            campaignId,
+            false
+        );
+
+        emit WiggyMint(msg.sender, tokenId, wiggyId);
+        emit IncreasePoint(msg.sender, numberPoints);
+    }
+
+    /**
+     * @notice Check if a user can claim.
+     */
+    function canClaim(address _userAddress) external view returns (bool) {
+        return _canClaim(_userAddress);
+    }
+
+    /**
+     * @notice Check if a user can claim.
+     */
+    function _canClaim(address _userAddress) internal view returns (bool) {
+        if (hasClaimed[_userAddress] || block.timestamp > endBlockTime) {
+            return false;
+        } else {
+            if (!wigoGalaxy.getResidentStatus(_userAddress)) {
+                return false;
+            } else {
+                if (token.balanceOf(_userAddress) >= thresholdBalance) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+}
